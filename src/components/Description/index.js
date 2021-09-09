5d3d540a1302278d1dba9e4996e38f5f6711e04d
Ariana Mestel <arianamestel@yahoo.com>
2021-09-09 15:45:18 -0400

    bolder

--- src/components/Description/index.js
+++ src/components/Description/index.js
@@ -4,7 +4,7 @@ import StockImage from "../../assets/background.jpg";
 export const Description = () => {
   return (
     <div className="box-border">
-      <div className="description mt-2 sm:mt-6 md:mt-8 text-bold">
+      <div className="description mt-2 sm:mt-6 md:mt-8">
         <h1 className="text-3xl sm:text-5xl md:text-7xl text-purple-600 gray-text-shadow">
           Ariana Mestel
         </h1>
