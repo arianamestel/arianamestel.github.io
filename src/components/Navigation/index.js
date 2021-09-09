b7329dc8ac2f0981367218a9851ae70ee2b7a85d
Ariana Mestel <arianamestel@yahoo.com>
2021-09-08 22:46:34 -0400

    icon links

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -31,10 +31,18 @@ export const NavBar = () => {
 
               <div className="absolute inset-y-0 left-0 flex items-center text-white text-lg">
                 <span className="mr-3 text-2xl">AM</span>
-                <a href="https://github.com/arianamestel">
+                <a
+                  href="https://github.com/arianamestel"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <i className="fab fa-github-square text-white mr-3 hover:text-purple-600 text-2xl"></i>
                 </a>
-                <a href="https://www.linkedin.com/in/ariana-mestel-b20425151/">
+                <a
+                  href="https://www.linkedin.com/in/ariana-mestel-b20425151/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <i className="fab fa-linkedin text-white mr-3 hover:text-purple-600 text-2xl"></i>
                 </a>
                 <a href="mailto:arianamestel@yahoo.com">
