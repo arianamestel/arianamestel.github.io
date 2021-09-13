7079893b1e88cf386915bcbd61783672c2d39560
Ariana Mestel <arianamestel@yahoo.com>
2021-09-12 20:33:01 -0400

    added google api to portfolio

--- src/containers/Portfolio/PortfolioItem.js
+++ src/containers/Portfolio/PortfolioItem.js
@@ -1,31 +1,43 @@
 import React from "react";
 
-export const PortfolioItem = ({ href, name, img, description }) => {
+export const PortfolioItem = ({
+  href,
+  name,
+  img,
+  description,
+  leftAligned = false,
+}) => {
+  const containerClass = leftAligned
+    ? "flex flex-wrap md:flex-nowrap mb-10"
+    : "flex flex-row-reverse flex-wrap md:flex-nowrap mb-10";
+  const imageClass = leftAligned ? "w-4/5 mx-auto" : " w-4/5 mx-auto";
   return (
-    <>
-      <div className="w-4/5 m-auto">
-        <div className="relative text-center">
-          <a href={href} target="_blank" rel="noreferrer">
-            <img
-              src={img}
-              alt={name}
-              className="shadow-xl h-full	w-full relative block"
-            />
-            <div className="portfolioOverlay hover:opacity-100 flex">
-              <button className="m-auto bg-white hover:bg-purple-500 text-purple-700 font-semibold hover:text-white py-2 px-4 border-2 border-purple-500 hover:border-transparent rounded">
-                Visit site
-              </button>
-            </div>
-          </a>
+    <div className={containerClass}>
+      <div className="md:w-1/2">
+        <div className={imageClass}>
+          <div className="relative text-center">
+            <a href={href} target="_blank" rel="noreferrer">
+              <img
+                src={img}
+                alt={name}
+                className="shadow-xl h-full	w-full relative block mb-5"
+              />
+              <div className="portfolioOverlay hover:opacity-100 flex">
+                <button className="m-auto bg-white hover:bg-purple-500 text-purple-700 font-semibold hover:text-white py-2 px-4 border-2 border-purple-500 hover:border-transparent rounded">
+                  Visit site
+                </button>
+              </div>
+            </a>
+          </div>
         </div>
       </div>
-      <div className="my-auto">
+      <div className="md:w-1/2 my-auto">
         <a href={href} target="_blank" rel="noreferrer">
           <p className="mb-1 text-purple-600 text-xl hover:underline">{name}</p>
         </a>
         <p>{description}</p>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/containers/Portfolio/index.js
+++ src/containers/Portfolio/index.js
@@ -1,22 +1,30 @@
 import React from "react";
 import PortfolioItem from "./PortfolioItem";
 import TheDebateImage from "../../assets/theDebateImage.png";
+import GoogleAPIImage from "../../assets/googleAPIIMage.png";
 
 export const Portfolio = ({ scrollRef }) => {
   return (
-    <div id="portfolio" className="w-5/6 mx-auto mt-16 pb-32" ref={scrollRef}>
+    <div id="portfolio" className="w-5/6 mx-auto mt-16 pb-16" ref={scrollRef}>
       <h1 className="text-4xl text-gray-800 text-center	mb-8">Portfolio</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-flow gap-4">
-        <PortfolioItem
-          name="The Debate"
-          description="The Debate is a blogging site for students to post articles and have
+      <PortfolioItem
+        name="The Debate"
+        description="The Debate is a blogging site for students to post articles and have
           discussions about interesting topics. The application includes user
           authentication and the ability to write, edit, and destroy
           posts or comments created by the user. The Debate was built using NodeJs, Express, EJS, HTML, CSS, and MongoDB."
-          img={TheDebateImage}
-          href="https://debate-blog.herokuapp.com/"
-        />
-      </div>
+        img={TheDebateImage}
+        href="https://debate-blog.herokuapp.com/"
+        leftAligned
+      />
+      <PortfolioItem
+        name="Google Search API"
+        description="This site leverages the Google Search API to retrieve search results based 
+          on the inputted keyword. The user can select, save, and download the results in JSON, CSV, 
+          or XML format. This site was built using React."
+        img={GoogleAPIImage}
+        href="https://am-google-api.herokuapp.com/"
+      />
     </div>
   );
 };
